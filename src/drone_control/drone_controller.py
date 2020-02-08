98ffbe17da5311397f3597015316fba1ef848276
Matthew Baugh <mb4617@ic.ac.uk>
2020-02-08 21:13:56 +0000

    Add mock drone option

--- src/drone_control/drone_controller.py
+++ src/drone_control/drone_controller.py
@@ -4,55 +4,67 @@ from threading import Thread
 
 
 class DroneController():
-    def __init__(self, mac_address, debug=False):
+    def __init__(self, mac_address, debug=False, mock=False):
         self.flying = False
         self.commands = Queue()
-        self.drone = Mambo(mac_address)
+        if not mock:
+            self.drone = Mambo(mac_address)
         self.debug = debug
+        self.mock = mock
+        
+        def done():
+            self.drone.safe_land(5)
+            self.flying = False
+
+        self.code_to_command = {
+            0: (lambda: done()),
+            1: (lambda: self.drone.safe_takeoff(5)),  # takeoff
+            2: (lambda: self.drone.safe_land(5)),  # land
+            3: (lambda: self.drone.fly_direct(0, 10, 0, 0, 1)),  # forward
+            4: (lambda: self.drone.fly_direct(0, -10, 0, 0, 1)),  # backward
+            5: (lambda: self.drone.fly_direct(-10, 0, 0, 0, 1)),  # left
+            6: (lambda: self.drone.fly_direct(10, 0, 0, 0, 1)),  # right
+            7: (lambda: self.drone.fly_direct(0, 0, 0, 10, 1)),  # up
+            8: (lambda: self.drone.fly_direct(0, 0, 0, -10, 1)),  # down
+            9: (lambda: self.drone.turn_degrees(-45)),  # turn left
+            10: (lambda: self.drone.turn_degrees(45)),  # turn right
+            11: (lambda: self.drone.flip("front")),  # flip forward
+            12: (lambda: self.drone.flip("back")),  # flip backward
+            13: (lambda: self.drone.flip("right")),  # flip right
+            14: (lambda: self.drone.flip("left")),  # flip left
+        }
+        if mock:
+            self.process_command = lambda c: print(F"Mock execute: {c}")
+        else:
+            self.process_command = lambda c: self.code_to_command[c.command_code]()
 
     def start_flight(self):
         self.flying = True
 
         def fly():
             self.flying = True
-            self.drone.connect(5)
-            self.drone.smart_sleep(3)
+            if not self.mock:
+                self.drone.connect(5)
+                self.drone.smart_sleep(5)
 
             while self.flying:
                 try:
                     c = self.commands.get(block=False)
                 except:
-                    self.drone.hover()
-                    self.drone.smart_sleep(1)
+                    if not self.mock:
+                        #self.drone.hover()
+                        self.drone.smart_sleep(2)
                     continue
 
                 if self.debug:
-                    print(c)
+                    print(F"Debug: {c}")
 
-                def done():
-                    self.drone.safe_land(5)
-                    self.flying = False
-                code_to_command = {
-                    0: (lambda: done()),
-                    1: (lambda: self.drone.safe_takeoff(5)),  # takeoff
-                    2: (lambda: self.drone.safe_land(5)),  # land
-                    3: (lambda: self.drone.fly_direct(0, 10, 0, 0, 1)),  # forward
-                    4: (lambda: self.drone.fly_direct(0, -10, 0, 0, 1)),  # backward
-                    5: (lambda: self.drone.fly_direct(-10, 0, 0, 0, 1)),  # left
-                    6: (lambda: self.drone.fly_direct(10, 0, 0, 0, 1)),  # right
-                    7: (lambda: self.drone.fly_direct(0, 0, 0, 10)),  # up
-                    8: (lambda: self.drone.fly_direct(0, 0, 0, -10)),  # down
-                    9: (lambda: self.drone.turn_degrees(-45)),  # turn left
-                    10: (lambda: self.drone.turn_degrees(45)),  # turn right
-                    11: (lambda: self.drone.flip("front")),  # flip forward
-                    12: (lambda: self.drone.flip("back")),  # flip backward
-                    13: (lambda: self.drone.flip("right")),  # flip right
-                    14: (lambda: self.drone.flip("left")),  # flip left
-                }
-                code_to_command[c.command_code]()
-                self.drone.smart_sleep(3)
+                self.process_command(c)
+                if not self.mock:
+                    self.drone.smart_sleep(3)
 
-            self.drone.disconnect()
+            if not self.mock:
+                self.drone.disconnect()
         t = Thread(target=fly)
         t.start()
 
--- src/test_drone_control.py
+++ src/test_drone_control.py
@@ -2,7 +2,7 @@ import drone_control as dc
 import drone_control.commands as commands
 
 
-controller = dc.get_drone_controller(debug=True)
+controller = dc.get_drone_controller(debug=True, mock=True)
 input("Are you ready kids?? ") # aye aye
 controller.start_flight()
 translate_commands = {
@@ -14,11 +14,23 @@ translate_commands = {
     "backward": commands.Backward,
     "left": commands.Left,
     "right": commands.Right,
+    "up": commands.Up,
+    "down": commands.Down,
+    "turnl": commands.TurnLeft,
+    "turnr": commands.TurnRight,
+    "flip": commands.FlipForward,
+    "flipf": commands.FlipForward,
+    "flipb": commands.FlipBackward,
+    "flips": commands.FlipRight,
+    
 }
 while True:
     c = input("Command: ")
     c = c.lower()
-    controller.send_command(translate_commands[c]())
+    try:
+        controller.send_command(translate_commands[c]())
+    except:
+        print("Invalid command")
     if c == "stop":
         break
 print("Flight over")
