893897e0f01c86dbaf617dd66567eb7eacd1daaa
Darrekt <darrick.lau15@imperial.ac.uk>
2020-02-08 18:25:58 +0000

    Refactor temp file into game

--- src/game_logic.py
+++ src/game_logic.py
@@ -2,12 +2,36 @@ from command import Commands
 
 class Game:
     def __init__(self, command_queue):
-        self.satisfaction = 100
+        self.session_list = []
         self.command_queue = command_queue
         self.commands = Commands()
     
+    def game_loop(self): 
+    # pops actions from the queue every x milliseconds and passes them to the existing session
+        pass
+
+    def start_game(self):
+    # Starts a session and adds it to the session list once it terminates
+        pass
+
+    def calc_game_stats(self):
+    # For twitter stuff after the sessions finish
+        pass
+
+    def tweet_stats(self):
+    # For twitter stuff after the sessions finish
+        pass
+
+    # REMOVE ME
     def do_it(self):
         while True:
             item = self.command_queue.get()
             print('get')
             print(item)
+
+
+class Session: 
+    def __init__(self):
+        self.player_name = ""
+        self.isActive = False
+        self.satisfaction = 40
