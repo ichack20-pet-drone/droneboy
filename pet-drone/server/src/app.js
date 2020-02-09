3caa61fbb44e77a5332184af9e74cc58622ccb22
Kitty Gu <kexinkittygu@gmail.com>
2020-02-09 06:38:18 +0000

    Server socket setup

--- pet-drone/server/src/app.js
+++ pet-drone/server/src/app.js
@@ -16,26 +16,27 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
+const server = app.listen(port);
 console.log(`App listening on ${port}`);
 
+const io = require("socket.io")(server);
+
 app.use("/", express.static(path.resolve("../../dist")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("../../dist/index.html"));
 });
 
-app.post("/api", function(req, res) {
-  console.log(req.body);
-  res.send("Got it");
-});
-
-const server = app.listen(port);
-
-const io = require("socket.io")(server);
-
+const clients = [];
 io.on("connection", function(socket) {
   console.log(socket.id);
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", function(data) {
-    console.log(data);
+  clients.push(socket.id);
+});
+
+app.post("/api", function(req, res) {
+  console.log(req.body);
+  clients.forEach(c => {
+    io.to(c).emit("UPDATE", { data: req.body });
   });
+  res.send("Got it");
 });
